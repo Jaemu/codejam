d5cdbe7cda75d108b1168d88171d162b3af49cda
Jill Munson <jillian.munson@viacom.com>
2015-06-17 21:03:53 -0400

    1.2

---
+++ ctci/js/1.2.js
@@ -0,0 +1,24 @@
+//Write code to reverse a C-Style String. 
+//(C-String means that “abcd” is represented as five characters, including the null character.)
+
+//Note - there isn't really a C-style string in javascript.  In chrome, ''.charCodeAt(0) is NaN but '\0'.charCodeAt(0) is 0
+//For super noodly weirdness, see what happens with:
+//'\r\n\t' == false
+//true
+//'\r\n\t' === false
+//false
+//
+//
+//Moving on...
+//Method 1:
+function reverseString(testStr){
+	var index = testStr.length;
+	var result = [];
+	for(index; index>-1; index--){
+		result.push(testStr[index]);
+	}
+	return result;
+}
+
+console.assert(reverseString('hello').length === 6);
+console.assert(reverseString('hello').join('') === 'olleh');
